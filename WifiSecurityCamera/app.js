2d76271a1896a54f8523fbb44452e688c4239ee3
medcv <y_shirvany@yahoo.com>
2015-07-20 15:42:55 +0200

    Update app.js

--- WifiSecurityCamera/app.js
+++ WifiSecurityCamera/app.js
@@ -1,8 +1,6 @@
 var express = require('express');
-var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
@@ -28,7 +26,6 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
